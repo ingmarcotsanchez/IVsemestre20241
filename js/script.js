321631414db890e743846e08f83763dd126a56b8
MARCO TULIO SANCHEZ ESPINOSA <ing.marcotsanchez@gmail.com>
2024-05-04 07:38:19 -0500

    primer commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 60)
+});
+
+/*luego de las mediaquerys*/
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
